36303b9591a934c54b80f54b24ce2f361427118a
srividyaghanta534 <srividyaghanta99@gmail.com>
2021-04-05 12:29:02 +0530

    main

--- multifile/header.h
+++ multifile/header.h
@@ -2,5 +2,11 @@
 #define __HEADER_H__
 
 int sum(int number1, int number2);
+/**
+ * @brief sum pf two numberz
+ * 
+ * @param number 
+ * @return int 
+ */
 int square(int number);
 #endif /* __HEADER_H__*/
